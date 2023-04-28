c55a412e51dbdb9f8a29b961ca093a7331248eff
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-04-28 20:44:33 +0200

    [TASK] create my first generic functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import * as FrontEnd from "./classes/Worker";
 import persons from "./data/person";
+import { printObject, genericFunction, genericFunctionArrow } from "../generics/generics";
 
 const worker1 = new FrontEnd.Worker( 'Alex', 'Alex Top', 43, 3687, 'IT');
 
 console.log(worker1.person);
+
+printObject( 1234 );
+printObject( new Date() );
+printObject( { a:1, b:2, c:3, d:4} );
+printObject( [1,2,3,4,5,6,7,8] );
+printObject( 'My first generic' );
+
+
+console.log(genericFunction( 1234 ).toFixed(2) );
+console.log(genericFunction( new Date() ).toLocaleDateString() );
+console.log(genericFunction( 'My first generic' ).toUpperCase() );
+
+console.log(genericFunctionArrow( 1234 ).toFixed(2) );
+console.log(genericFunctionArrow( new Date() ).toLocaleDateString() );
+console.log(genericFunctionArrow( 'My first generic' ).toUpperCase() );
